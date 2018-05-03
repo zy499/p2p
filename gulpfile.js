b5c39535ddfeae86c876f05b3f0566052fcd13a7
zy499 <hqh499@gmail.com>
2018-05-03 19:08:15 +0800

    项目搭建完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,51 @@
 var gulp = require('gulp');//引入gulp，管理插件由gulp开启任务
+//引入路径模块
+var path = require("path");
 var uglify = require('gulp-uglify');//js压缩插件
 var rename = require('gulp-rename');//重命名插件
 var clean = require('gulp-clean-css');//css压缩
 var less = require('gulp-less');//less编译
 var livereload = require('gulp-livereload')//热刷新插件
+//引入资源地图模块
+var sourceMaps = require('gulp-sourcemaps');
+// 引入Less插件模块 
+var sass =require('gulp-sass');
 // var webserver = require('gulp-webserver');//浏览器自动打开能开启热刷新
 /* 
     gulp开启任务 gulp.task('参数 任务名称',callback)
     gulp可以链式操作
 */
-/* gulp.task('uglify', function () {
+gulp.task('less',function(){
     gulp
-        .src('./js/index.js')
+        .src('./public/src/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./public/src/css'))
+});
+gulp.task('sass',function(){
+    gulp
+        .src('./public/src/sass/*.sass')
+        .pipe(less())
+        .pipe(gulp.dest('./public/src/css'))
+});
+gulp.task('uglify', function () {
+    gulp
+        .src('./public/src/js/index.js')
         .pipe(uglify())
         .pipe(rename({
             suffix:'.min'
         }))
-        .pipe(gulp.dest('./dist/js'))
+        .pipe(gulp.dest('./public/dist/js'))
 });
 gulp.task('clean',function(){
     gulp
-        .src('./styles/css/index.css')
+        .src('./public/src/css/index.css')
         .pipe(clean())
         .pipe(rename({
             suffix:'.min'
         }))
-        .pipe(gulp.dest('./dist/styles/css'))
-}) */
-gulp.task('less',function(){
-    gulp
-        .src('./public/src/less/index.less')
-        .pipe(less())
         .pipe(gulp.dest('./public/dist/css'))
-});
-// gulp.task('clean',function(){
-//     gulp
-//         .src('./styles/css/index.css')
-//         .pipe(clean())
-//         .pipe(rename({
-//             suffix:'.min'
-//         }))
-//         .pipe(gulp.dest('./dist/css'))
-// });
+})
+
 //热刷新插件可以开启服务器
 /* gulp.task('webserver',function(){
     gulp
@@ -53,8 +57,8 @@ gulp.task('less',function(){
 
 }) */
 gulp.task('default',function(){
-    gulp.watch('./public/src/less/index.less',['less'])
-    // gulp.watch('./styles/css/index.css',['clean'])
+    gulp.watch('./public/src/**/*',['less','sass'])
+    gulp.watch('./public/src/css/*.css',['clean'])
 })
 /* 
 任务依赖，当前任务开启后，先运行依赖任务

a069771f887fdead57aa1bd6d70eb0d26a996bc3
zy499 <hqh499@gmail.com>
2018-05-09 13:16:48 +0800

    个人中心正文交互效果

--- public/dist/js/p2pDispose.js
+++ public/dist/js/p2pDispose.js
@@ -2,4 +2,15 @@
 $(function () {
     //工具显示微信图标
     $('#weixinTips').tooltip();
+    //个人中心正文导航条收齐与展开
+    $('#toggleBtn').on('click',function(){
+        // alert(1)
+        $('#memberMain').toggleClass('active');
+        let isActive = $('#memberMain').hasClass('active');
+        if(isActive){
+            $(this).text('展开');
+        }else{
+            $(this).text('收起');
+        }
+    });
 });
